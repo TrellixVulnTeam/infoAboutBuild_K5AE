2792b44bf38a2f298cfe00cc770af9d2d4c11b2f
Danya-hub <6694524@gmail.com>
2021-08-12 15:19:19 +0300

    add changing

--- src/app/js/common/animHead.js
+++ src/app/js/common/animHead.js
@@ -1,1 +1,7 @@
-export default (() => window.addEventListener('scroll', () => document.getElementById('topPanel').style.background = (window.pageYOffset != 0 ? '#4e02d1' : '#0000')))
+export default (() => {
+    const header = document.getElementById('topPanel');
+    window.addEventListener('scroll', () => {
+        header.style.cssText = (window.pageYOffset != 0 ? `background: #fff; box-shadow: 0 0 4px rgba(0, 0, 0, 0.5)` : null);
+        header.querySelectorAll('svg, a').forEach(e => e.style.cssText = (window.pageYOffset != 0 ? `color: #000; fill: #000;` : null));
+    })
+})
